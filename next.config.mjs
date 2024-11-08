9f8b3b9e504c7d164a38ac5243cced0b75a5d66b
nabdi7 <36966910+nabdi7@users.noreply.github.com>
2024-11-08 14:40:40 -0800

    minor changes

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,18 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+const nextConfig = {
+    reactStrictMode: true,
+    output: 'export',
+    images: {unoptimized: true},
+    // images: {
+    //   remotePatterns: [
+    //     {
+    //       protocol: "https",
+    //       hostname: "res.cloudinary.com",
+    //       port: "",
+    //     },
+    //   ],
+    // },
+  };
+  
+  export default nextConfig;
+  
